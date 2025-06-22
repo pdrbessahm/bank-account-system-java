488c1fd6e34a042df3b260ebe822cd6ae5615f63
pdrbessahm <bessapedro500@gmail.com>
2025-06-22 01:12:53 -0300

    Last changes

--- src/App.java
+++ src/App.java
@@ -31,7 +31,7 @@ public class App {
             switch(choice){
                 case 1 -> showBalance(balance);
                 case 2 -> balance = balance + deposit();
-                case 3 -> System.out.println("WITHDRAW");
+                case 3 -> balance = balance - withdraw(balance);
                 case 4 -> isRunning = false;
                 default -> System.out.println("INVALID CHOICE. TYPE AGAIN.");
 
@@ -46,6 +46,9 @@ public class App {
        // withdraw()
 
        // EXIT MESSAGE
+       System.out.println("**********************");
+       System.out.println("Thank you! Have a nice day");
+       System.out.println("**********************");
 
        
     }
@@ -68,5 +71,23 @@ public class App {
             return amount;
         }
 
+    }
+    static double withdraw(double balance){
+
+        double amount;
+
+        System.out.print("Enter amount to be withdrawn: ");
+        amount = scanner.nextDouble();
+
+        if(amount > balance){
+            System.out.println("INSUFFICIENT FUNDS");
+
+        } else if(amount < 0){
+            System.out.println("AMOUNT CAN'T BE NEGATIVE");
+
+        } else {
+            return amount;
+        }
+        return 0;
     }
 }
