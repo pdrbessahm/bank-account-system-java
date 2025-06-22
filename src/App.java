f1d2689a4dd074636ba3f7fb30e4a769ed810748
pdrbessahm <bessapedro500@gmail.com>
2025-06-22 00:56:33 -0300

    changes

--- src/App.java
+++ src/App.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
 public class App {
+
+    static Scanner scanner = new Scanner(System.in); // AVOIDING CREATE A LOT OF SCANNERS
     public static void main(String[] args) throws Exception {
        // JAVA BANKING PROGRAM
 
        // DECLARE VARIABLES 
-        Scanner scanner = new Scanner(System.in); 
-        double balance; // Because it includes decimal numbers here
+        // USE TO BE A SCANNER HERE
+        double balance = 0; // Because it includes decimal numbers here
         boolean isRunning = true;
         int choice;
 
@@ -27,8 +29,8 @@ public class App {
         
             // SWITCH HERE ITS AN EASIER WAY TO SET THE CHOICE SYSTEM RATHER THAN IF CHAIN CODE    
             switch(choice){
-                case 1 -> System.out.println("SHOW BALANCE");
-                case 2 -> System.out.println("DEPOSIT");
+                case 1 -> showBalance(balance);
+                case 2 -> balance = balance + deposit();
                 case 3 -> System.out.println("WITHDRAW");
                 case 4 -> isRunning = false;
                 default -> System.out.println("INVALID CHOICE. TYPE AGAIN.");
@@ -39,14 +41,32 @@ public class App {
 
        // GET AND PROCESS USERS CHOICE
 
-       // showBalance()
-
        // deposit()
 
        // withdraw()
 
        // EXIT MESSAGE
 
-       scanner.close();
+       
+    }
+    // showBalance()
+    static void showBalance(double balance){
+        System.out.println("***************");
+        System.out.printf("R$%.2f\n", balance);
+    }
+    static double deposit(){
+        
+        double amount;
+
+        System.out.print("Enter an amount to be deposited: ");
+        amount = scanner.nextDouble();
+
+        if(amount < 0){
+            System.out.println("Amount can't be negative.");
+            return 0;
+        } else {
+            return amount;
+        }
+
     }
 }
